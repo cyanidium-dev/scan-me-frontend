bb2aed9a1d3486b5248a1aa978546d090a7f5f19
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-22 17:27:41 +0200

    added animations

--- src/components/homePage/mission/Mission.tsx
+++ src/components/homePage/mission/Mission.tsx
@@ -1,20 +1,43 @@
 import Container from "@/components/shared/container/Container";
 import { useTranslations } from "next-intl";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
 
 export default function Mission() {
   const t = useTranslations("homePage.mission");
 
   return (
-    <section className="py-6 lg:py-12 mt-22 lg:mt-30 bg-black rounded-[16px] text-white">
+    <motion.section
+      initial="hidden"
+      whileInView="visible"
+      exit="exit"
+      viewport={{ once: true, amount: 0.1 }}
+      variants={fadeInAnimation({})}
+      className="py-6 lg:py-12 mt-22 lg:mt-30 bg-black rounded-[16px] text-white"
+    >
       <Container className="flex flex-col lg:flex-row gap-6 lg:gap-20 xl:gap-50">
-        <p className="lg:w-[500px] xl:w-[570px] text-[12px] lg:text-[14px] font-semibold leading-[120%] uppercase shrink-0">
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.1 }}
+          variants={fadeInAnimation({ scale: 0.95, x: -30 })}
+          className="lg:w-[500px] xl:w-[570px] text-[12px] lg:text-[14px] font-semibold leading-[120%] uppercase shrink-0"
+        >
           {t("title")}
-        </p>
-        <div className="flex gap-3 items-center">
+        </motion.p>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.1 }}
+          variants={fadeInAnimation({ scale: 0.95, x: 30 })}
+          className="flex gap-3 items-center"
+        >
           <div className="w-[3px] h-[34px] bg-accent rounded-l-[2px]" />
           <p>{t("description")}</p>
-        </div>
+        </motion.div>
       </Container>
-    </section>
+    </motion.section>
   );
 }
--- src/utils/animationVariants.ts
+++ src/utils/animationVariants.ts
@@ -70,3 +70,18 @@ export const listItemVariants = {
     transition: { duration: 0.7, ease: [0.42, 0, 1, 1] as const },
   },
 };
+
+export const listItemVariantsLeft = {
+  hidden: { opacity: 0, x: 20, scale: 0.95 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    scale: 1,
+    transition: { duration: 0.7, ease: [0.25, 0.1, 0.25, 1] as const },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.95,
+    transition: { duration: 0.7, ease: [0.42, 0, 1, 1] as const },
+  },
+};
