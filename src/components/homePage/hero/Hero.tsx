f86b94595e392907f7b5fd2c4da38d78a95164d4
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-22 12:31:32 +0200

    added animations

--- src/components/homePage/hero/Hero.tsx
+++ src/components/homePage/hero/Hero.tsx
@@ -4,6 +4,8 @@ import PageTitle from "@/components/shared/titles/PageTitle";
 import DecorativeEllipsis from "@/components/shared/decorativeEllipsis/DecorativeEllipsis";
 import MainButton from "@/components/shared/buttons/MainButton";
 import Image from "next/image";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
 
 export default function Hero() {
   const t = useTranslations("homePage.hero");
@@ -16,20 +18,50 @@ export default function Hero() {
             accent: (chunks) => <span className="text-accent">{chunks}</span>,
           })}
         </PageTitle>
-        <p className="relative lg:absolute lg:top-[220px] xl:top-[252px] right-15 z-10 text-[10px] lg:text-[14px] font-semibold leading-[120%] uppercase lg:text-right max-w-[132px] lg:max-w-[325px] mb-[76px] lg:mb-0">
-          {t("descriptionOne")}
-        </p>
-        <DecorativeEllipsis className="relative lg:absolute lg:top-[172px] xl:top-[182px] right-15 z-10 mb-[76px]" />
-        <p className="relative z-10 max-w-[313px] lg:max-w-[415px] mb-4 lg:mb-16 text-[10px] lg:text-[12px] xl:text-[14px] font-light leading-[120%]">
-          {t("descriptionTwo")}
-        </p>
-        <MainButton
-          variant="gradient"
-          className="relative z-10 w-full lg:w-[282px] h-[54px]"
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.3 }}
+          variants={fadeInAnimation({ scale: 0.85, y: 30, x: 70 })}
+          className="relative lg:absolute lg:top-[220px] xl:top-[252px] lg:right-15 z-10 text-[10px] lg:text-[14px] font-semibold leading-[120%] uppercase lg:text-right max-w-[132px] lg:max-w-[325px] mb-[76px] lg:mb-0"
         >
-          {t("button")}
-        </MainButton>
-        <div className="absolute top-[77px] lg:top-[113px] left-6 sm:left-36 md:left-50 lg:left-[409px] w-[475px] h-[542px] lg:w-[885px] lg:h-[708px] overflow-hidden">
+          {t("descriptionOne")}
+        </motion.p>
+        <DecorativeEllipsis className="relative lg:absolute lg:top-[172px] xl:top-[182px] lg:right-15 z-10 mb-[76px]" />
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.3 }}
+          variants={fadeInAnimation({ scale: 0.85, y: 30, x: -70, delay: 0.4 })}
+          className="relative z-10 max-w-[313px] lg:max-w-[415px] mb-4 lg:mb-16 text-[10px] lg:text-[12px] xl:text-[14px] font-light leading-[120%]"
+        >
+          {t("descriptionTwo")}
+        </motion.p>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.3 }}
+          variants={fadeInAnimation({ scale: 0.85, y: 30, delay: 0.8 })}
+          className="relative z-10 w-full lg:w-fit"
+        >
+          <MainButton
+            variant="gradient"
+            className="w-full lg:w-[282px] h-[54px] h-[54px]"
+          >
+            {t("button")}
+          </MainButton>
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.3 }}
+          variants={fadeInAnimation({ scale: 0.95, delay: 0.2 })}
+          className="absolute top-[77px] lg:top-[113px] left-6 sm:left-36 md:left-50 lg:left-[409px] w-[475px] h-[542px] lg:w-[885px] lg:h-[708px] overflow-hidden"
+        >
           <Image
             src="/images/homePage/hero/heroMob.webp"
             alt="hero image"
@@ -39,15 +71,16 @@ export default function Hero() {
             className="lg:hidden object-cover"
           />
           <Image
-            src="/images/homePage/hero/heroDesk1.webp"
+            src="/images/homePage/hero/heroDesk.webp"
             alt="hero image"
             fill
             priority
             fetchPriority="high"
+            quality={100}
             className="hidden lg:block object-cover"
           />
           <div className="absolute bottom-0 lg:bottom-[-190px] left-[-50px] lg:left-[-297px] w-[671px] lg:w-[575px] h-[175px] lg:h-[478px] lg:-rotate-14 bg-black rounded-full supports-[backdrop-filter]:blur-[39px] lg:supports-[backdrop-filter]:blur-[109px] will-change-transform" />
-        </div>
+        </motion.div>
       </Container>
     </section>
   );
--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,23 +1,27 @@
 "use client";
 import { useState } from "react";
-import { useTranslations } from "next-intl";
-import MainButton from "../buttons/MainButton";
 import Container from "../container/Container";
 import Logo from "../logo/Logo";
 import NavMenu from "./navMenu/NavMenu";
 import LocaleSwitcher from "./LocaleSwitcher";
-import { Link } from "@/i18n/navigation";
 import Image from "next/image";
 import BurgerMenu from "./burgerMenu/BurgerMenu";
 import AuthButtons from "./AuthButtons";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
 
 export default function Header() {
-  const t = useTranslations("header");
-
   const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
 
   return (
-    <header className="fixed z-10 top-0 left-0 w-dvw py-6 lg:py-11 bg-black rounded-b-2xl text-white">
+    <motion.header
+      initial="hidden"
+      whileInView="visible"
+      exit="exit"
+      viewport={{ once: true, amount: 0.1 }}
+      variants={fadeInAnimation({})}
+      className="fixed z-20 top-0 left-0 w-dvw py-6 lg:py-11 bg-black rounded-b-2xl text-white"
+    >
       <div className="absolute z-50 top-0 left-0 h-full w-full overflow-hidden">
         <div
           className="absolute top-[-115px] lg:top-[-104px] left-[-95px] sm:left-[calc(50%-320px-95px)] md:left-[calc(50%-384px-95px)] 
@@ -46,6 +50,6 @@ export default function Header() {
           />
         </div>
       </Container>
-    </header>
+    </motion.header>
   );
 }
