75a5e99231474c9c8c55c45ec7a9919b14b6d2f7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-23 10:23:02 +0200

    added steps list

--- src/components/homePage/howItWorks/HowItWorks.tsx
+++ src/components/homePage/howItWorks/HowItWorks.tsx
@@ -1,9 +1,22 @@
 import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+import { useTranslations } from "next-intl";
+import StepsList from "./StepsList";
 
 export default function HowItWorks() {
+  const t = useTranslations("homePage.howItWorks");
+
   return (
-    <section className="pt-12 lg:pt-30">
-      <Container></Container>
+    <section id="how-it-works" className="pt-12 lg:pt-30">
+      <Container>
+        <SectionTitle className="mb-6">
+          {t.rich("title", {
+            accent: (chunks) => <span className="text-accent">{chunks}</span>,
+          })}
+        </SectionTitle>
+        <p className="max-w-[180px] mb-4 lg:mb-0">{t("descriptionOne")}</p>
+        <StepsList />
+      </Container>
     </section>
   );
 }
