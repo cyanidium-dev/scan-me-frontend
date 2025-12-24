7ea39c7715ae176264dedcd712952c3a88a1f0b4
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-24 10:58:57 +0200

    added texts

---
+++ src/components/homePage/contactUs/ContactUs.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/shared/container/Container";
+
+export default function ContactUs() {
+  return (
+    <section className="py-12 lg:py-30">
+      <Container></Container>
+    </section>
+  );
+}
