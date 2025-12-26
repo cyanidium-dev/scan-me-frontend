dc94895c59674b2f0c7147a6e5467de80b60aecc
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-26 13:25:55 +0200

    added socials block

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,11 @@
 export const EMAIL = "hello@scanme.ua";
 
 export const PHONE = "+380671234567";
+
+export const LINKEDIN_URL = "";
+
+export const INSTAGRAM_URL = "";
+
+export const TELEGRAM_URL = "";
+
+export const TIKTOK_URL = "";
