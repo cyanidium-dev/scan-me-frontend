4d2ac23dbfebf87ba01ca50aa7aa028017ab4381
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-12-29 11:47:55 +0200

    added decorations

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -6,23 +6,33 @@ import Socials from "../socials/Socials";
 import NavMenu from "./NavMenu";
 import FooterThumb from "./FooterThumb";
 import ContactUs from "./ContactUs";
+import Image from "next/image";
 
 export default function Footer() {
   const t = useTranslations("footer");
 
   return (
-    <footer className="w-full py-12 rounded-t-[16px] bg-black">
-      <Container>
+    <footer className="w-full py-12 rounded-t-[16px] bg-black overflow-hidden">
+      <Container className="relative z-10">
+        <div className="absolute top-[-169px] lg:top-[-115px] left-[-145px] lg:left-[-135px] -z-10">
+          <Image
+            src="/images/footer/fingerprint.webp"
+            width={309}
+            height={339}
+            className="object-cover"
+            alt="fingerprint"
+          />
+        </div>
         <div className="flex flex-col gap-12 sm:gap-6 sm:flex-row sm:justify-between">
           <div>
             <Logo />
-            <SectionTitle className="max-w-52 mt-6 mb-6 lg:mb-0 font-montserrat text-[14px] lg:text-[14px] xl:text-[14px] font-semibold leading-[120%] text-white">
+            <h2 className="max-w-52 mt-6 mb-6 lg:mb-0 text-[14px] lg:text-[14px] xl:text-[14px] font-semibold leading-[120%] text-white uppercase">
               {t.rich("title", {
                 accent: (chunks) => (
                   <span className="text-accent">{chunks}</span>
                 ),
               })}
-            </SectionTitle>
+            </h2>
             <Socials className="py-3 text-white" />
           </div>
           <div className="flex flex-col sm:flex-row gap-12 md:gap-20 lg:gap-40">
