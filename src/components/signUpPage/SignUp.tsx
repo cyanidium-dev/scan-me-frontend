d9e55170d0f5bb716483818354719dc492c53e70
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-01-08 12:29:06 +0200

    added sign up form initial step

---
+++ src/components/signUpPage/SignUp.tsx
@@ -0,0 +1,24 @@
+import { useTranslations } from "next-intl";
+import Container from "../shared/container/Container";
+import SignUpForm from "../shared/forms/SignUpForm";
+import SectionTitle from "../shared/titles/SectionTitle";
+import AnimatedAuthImage from "../shared/images/AnimatedAuthImage";
+
+export default function SignUp() {
+  const t = useTranslations("signInPage");
+
+  return (
+    <section className="pt-[147px] lg:pt-[205px] pb-15 lg:pb-[105px]">
+      <Container className="relative lg:flex lg:gap-20 xl:gap-[150px]">
+        <SignUpForm />
+        <SectionTitle className="relative z-10 hidden lg:block lg:mt-[-32px] xl:mt-[-49px] text-white lg:text-[26px] xl:text-[40px]">
+          {t.rich("qr", {
+            accent: (chunks) => <span className="text-accent">{chunks}</span>,
+          })}
+        </SectionTitle>
+        <AnimatedAuthImage />
+      </Container>
+    </section>
+  );
+}
+
